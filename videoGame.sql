82ded3b1c0a1090c393b2ae2c62a26a3b19ad879
Chris King <cking3@mail.umw.edu>
2013-02-25 17:38:24 -0500

    Added videoGame.sql

---
+++ videoGame.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS gamereviews;
+GRANT ALL PRIVILEGES ON videogames.* to 'vidgameuser'@'localhost' identified by 'videogameuser';
+USE gamereviews;
+
+CREATE TABLE videogames (
+  id int NOT NULL auto_increment,
+  gamename VARCHAR(25),
+  ESRBrating VARCHAR(20),
+  genre VARCHAR(15),
+  releasedate DATE,
+  score int,
+  replayability VARCHAR(25),
+  AdminReview BLOB,
+  picturelink VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE systems (
+	id int NOT NULL,
+	system VARCHAR(20)
+);
+
+CREATE TABLE reviews (
+	id int NOT NULL,
+	userID int,
+	userReview BLOB
+);
+
+CREATE TABLE users (
+	id int NOT NULL auto_increment,
+	userName VARCHAR(25) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	name VARCHAR(20) NOT NULL,
+	favConsole VARCHAR(
+);
+	
