7c4041d2539f2d9188940e2c20a5f4a524b3d165
Elisabeth <ena1234@hotmail.com>
2013-02-26 16:18:33 -0500

    Added template to searchResults.php

--- searchResults.php
+++ searchResults.php
@@ -4,13 +4,51 @@
 <head>
 	<title>Search Results</title>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-	<link rel="stylesheet" type="text/css" href="style/style.css" />
+	<link href="styles.css" rel="stylesheet" type="text/css" />
+<!--[if IE 5]>
+<style type="text/css"> 
+/* place css box model fixes for IE 5* in this conditional comment */
+#sidebar1 { width: 230px; }
+</style>
+<![endif]--><!--[if IE]>
+<style type="text/css"> 
+/* place css fixes for all versions of IE in this conditional comment */
+#sidebar1 { padding-top: 30px; }
+#mainContent { zoom: 1; }
+/* the above proprietary zoom property gives IE the hasLayout it needs to avoid several bugs */
+</style>
+<![endif]-->
 </head>
 
 <body>
-
-	<h1>Search Results</h1>
-	<?php
+<!-- begin #container -->
+<div id="container">
+	<!-- begin #header -->
+    <div id="header">
+    	<div class="logoContainer">
+            <div class="logo">Video Game <span>Reviews</span></div>
+            <div class="author"><a href="http://www.freewebsite-template.com"></a></div>
+        </div>
+        <!-- begin #navcontainer -->
+        <div id="navcontainer">
+            <ul id="navlist">
+                <li><a href="index2.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#">Member List</a></li>
+                <li><a href="#">Contact</a></li>
+				<li id="active"><a href="search.php" id="current">Search</a></li>
+            </ul>
+        </div>
+        <!-- end #navcontainer -->
+    </div>
+    <!-- end #header -->
+    <div class="headerPic"><h2>Online <span>GAMES</span> portal</h2></div>
+    <div class="sidebar3">
+    	<div class="titleBlock">Search Results</div>
+        <p>
+        	
+			<?php
 		include "dbconnect.php";
 		if(isset($_GET['searchterm'])){
 			$searchTerm = mysqli_real_escape_string($db, $_GET['searchterm']);
@@ -27,6 +65,24 @@
 		echo '</div>';
 		
 	?>
+			
+			
+			</p>
+    </div>
+    <br class="clearfloat" />
+</div>
+<!-- end #container -->
+<!-- begin #footer -->
+    <div id="footer">
+        <p>
+        Terms of Use |
+        <a title="This page validates as XHTML 1.0 Strict" href="http://validator.w3.org/check/referer" class="footerLink"><abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> | 
+        <a title="This page validates as CSS" href="http://jigsaw.w3.org/css-validator/check/referer" class="footerLink"><abbr title="Cascading Style Sheets">CSS</abbr></a><br />
+        Copyright &copy; Games Club. Designed by <a href="http://www.freewebsite-template.com" title="Free Website Templates">Free Website Templates</a>
+        </p>
+    </div>
+<!-- end #footer -->
+	
 
 
 
