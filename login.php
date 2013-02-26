dad40af5e6f0cd486d10e10bdd5e6ccee16d31f6
Brendan Prin <bprin@umw.edu>
2013-02-26 12:14:30 -0500

    Updated

--- login.php
+++ login.php
@@ -1,65 +1,87 @@
-<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN”
-   “http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd”>
-<html xmlns=”http://www.w3.org/1999/xhtml” xml:lang=”en” lang=”en”>
-   <head>
-      
-	  <?php
-  	   include "db_connect.php";
-  	   if (isset($_POST['userName'])){
-  	     $name = $_POST['userName'];
-         $pw = sha1($_POST['pw']);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Video Game Reviews</title>
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<!--[if IE 5]>
+<style type="text/css"> 
+/* place css box model fixes for IE 5* in this conditional comment */
+#sidebar1 { width: 230px; }
+</style>
+<![endif]--><!--[if IE]>
+<style type="text/css"> 
+/* place css fixes for all versions of IE in this conditional comment */
+#sidebar1 { padding-top: 30px; }
+#mainContent { zoom: 1; }
+/* the above proprietary zoom property gives IE the hasLayout it needs to avoid several bugs */
+</style>
+<![endif]--></head>
 
-         $query = "select * from users WHERE userName = '$name' AND password = '$pw'";
-         $result = mysqli_query($db, $query)
-         or die("Error Querying Database");
-         if ($row = mysqli_fetch_array($result))
-         {
-   		#echo $query;
-   		echo '<META http-equiv="refresh" content="0;URL=index.php">';
-       }}
-?>
-	  <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″ />
-      <title>Login</title>
-   </head>
-   <body>
-  <div id="main">
-    <div id="links"></div>
+<body>
+<!-- begin #container -->
+<div id="container">
+	<!-- begin #header -->
     <div id="header">
-      <div id="logo">
-        <div id="logo_text">
-          <h1>Harlem Cake<span class="alternate_colour"> Fails</span></h1>
+    	<div class="logoContainer">
+            <div class="logo">Video Game <span>Reviews</span></div>
+            <div class="author"><a href="http://www.freewebsite-template.com"></a></div>
         </div>
-      </div>
-      <div id="menubar">
-        <ul id="menu">
-          <li ><a href="index.php">Home</a></li>
-          <li class="tab_selected"><a href="login.php">Login</a></li>
-          <li><a href="search.php">Search for Game</a></li>
-		  <li><a href="about_us.html">About Us</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
-        </ul>
-      </div>
+        <!-- begin #navcontainer -->
+        <div id="navcontainer">
+            <ul id="navlist">
+                <li ><a href="index.php">Home</a></li>
+				<li class="tab_selected"><a href="login.php">Login</a></li>
+				<li><a href="search.php">Search for Game</a></li>
+				<li><a href="about_us.html">About Us</a></li>
+				<li><a href="contact.html">Contact Us</a></li>
+            </ul>
+        </div>
+        <!-- end #navcontainer -->
     </div>
-      
-      <div id="content">
-        <!-- insert the page content here -->
-    
-        <?php
-        if (isset($_POST['username'])) {
-        echo "<h2>Incorrect Username/Password</h2>";
-        }
-        ?>
-        
-        <h1>Login</h1>
-          <form method="post" action="login.php">
-    <p>
-    <label for="userName">Username:</label>
-    <input type="text" id="userName" name="userName" size="40" </p>
-    <p>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="pw" size="40" /></p>
+    <!-- end #header -->
+    <div class="headerPic"><h2>Online <span>GAMES</span> portal</h2></div>
+    <div class="sidebar1">
+    	<div class="titleBlock">About us</div>
+        <h1>Your Flash Site will be look professional!</h1>
+        <p>
+        An extensive number of unique Flash Templates is at your service. All templates on our site were created specially for you! Our independent designers created flash professional templates. Also our templates are unique. It means you can use our flash templates to create your own site!  
+        </p>
+        <p>
+        	Our studio offers a big choice of free flash templates categories for your business and personal needs. By using our unique products, you can create an excellent animated website and spend minimum time and money on it. Most of our templates are compatible with Flash MX2004, Flash 8, Flash CS3, and Flash CS4. You don’t have to be a professional flash developer to create a website.
+        </p>
+    </div>
+    <div class="sidebar2">
+    	<div class="titleBlock">Welcome to us</div>
+        <h1>All website template is released under a Creative Commons Avttribution 2.5 License</h1>
+        <p>
+        	We request you retain the full copyright notice below including the link to <a href="http://www.FreeWebsite-Template.com">www.FreeWebsite-Template.com</a>. This not only gives respect to the large amount of time given freely by the developers but also helps build interest, traffic and use of our free and paid designs. If you cannot (for good reason) retain the full copyright we request you at least leave in place the Website Templates line, with Website Templates linked to <a href="http://www.FreeWebsite-Template.com">www.FreeWebsite-Template.com</a>. If you refuse to include even this then support may be affected.
 
-    <p><input type="submit" value="login" name="submit" /></p>
-  </form>
-  
+You are allowed to use this design only if you agree to the following conditions:
+- You can not remove copyright notice from any our template without our permission.
+- If you modify any our template it still should contain copyright because it is based on our work.
+- You may copy, distribute, modify, etc. any our template as long as link to our website remains untouched.
+
+For support please visit <a href="http://FreeWebsite-Template.com/contact.php">http://FreeWebsite-Template.com/contact.php</a>
+        </p>
+        <br /><br />
+        <p>
+        	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,  sed diam nonumy<br /><br />
+            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        </p>
+    </div>
+    <br class="clearfloat" />
+</div>
+<!-- end #container -->
+<!-- begin #footer -->
+    <div id="footer">
+        <p>
+        Terms of Use |
+        <a title="This page validates as XHTML 1.0 Strict" href="http://validator.w3.org/check/referer" class="footerLink"><abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> | 
+        <a title="This page validates as CSS" href="http://jigsaw.w3.org/css-validator/check/referer" class="footerLink"><abbr title="Cascading Style Sheets">CSS</abbr></a><br />
+        Copyright &copy; Games Club. Designed by <a href="http://www.freewebsite-template.com" title="Free Website Templates">Free Website Templates</a>
+        </p>
+    </div>
+<!-- end #footer -->
+</body>
 </html>
