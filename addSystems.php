9c5a366ca98e0a6bab3242f62fcbc56eeea6e1cb
Elisabeth <ena1234@hotmail.com>
2013-02-28 12:11:49 -0500

    created a file to add systems to the games

---
+++ addSystems.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+
+<body>
+<?php
+include ('dbconnect.php');
+$query = "INSERT INTO systems (id, system) VALUES (2,'xbox 360')";
+$result = mysqli_query($db, $query)
+   			or die("Error Querying Database");
+echo "Finished adding system 1!";
+
+
+
+?>
+</body>
+</html>
