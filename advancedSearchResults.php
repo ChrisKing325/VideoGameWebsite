c13a678020f4eaa82cf5767d330ad6243962c9a8
Elisabeth <ena1234@hotmail.com>
2013-03-19 01:19:47 -0400

    Slightly more with the advanced search - broken still

--- advancedSearchResults.php
+++ advancedSearchResults.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-	<title>Advanced Search</title>
+	<title>Search Results</title>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 	<link href="styles.css" rel="stylesheet" type="text/css" />
 <!--[if IE 5]>
@@ -45,56 +45,31 @@
     <!-- end #header -->
     <div class="headerPic"><h2></h2></div>
     <div class="sidebar3">
-    	<div class="titleBlock">Advanced Search</div>
+    	<div class="titleBlock">Search Results</div>
         <p>
-        	<form method="get" action="advancedSearchResults.php">
-		Enter your search terms here: <br/>
-		Name of game: <input type="text" id="name" name="name" size="40"/><br/>
-		ESRB Rating: 
-			<select id="esrb" name="esrb">
-				<option value="none">Select...</option>
-				<option value="Early Childhood">EC - Early Childhood</option>
-				<option value="Everyone 10+">E10 - Everyone 10+</option>
-				<option value="Teen">T - Teen</option>
-				<option value="Mature">M - Mature</option>
-				<option value="Adults Only">A - Adults Only</option>
-				<option value="Rating Pending">RP - Rating Pending</option>
-			</select><br/>
-		Genre: <input type="text" id="genre" name="genre" size="40"/><br/>
-		Release Date:
-			<select id="month" name="month">
-				<option value="m">Month</option>
-				<option value="1">January</option>
-				<option value="2">February</option>
-				<option value="3">March</option>
-				<option value="4">April</option>
-				<option value="5">May</option>
-				<option value="6">June</option>
-				<option value="7">July</option>
-				<option value="8">August</option>
-				<option value="9">September</option>
-				<option value="10">October</option>
-				<option value="11">November</option>
-				<option value="12">December</option>
-			</select>
-			<select id="day" name="day">
-				<option value="d">Day</option>
-				<?php
-					for($i=1; $i<32; $i++){
-						echo "<option value=\"$i\">$i</option>\n";
-					}
-				?>
-			</select>
-			<select id="year" name="year">
-				<option value="y">Year</option>
-				<?php
-					for($i=2050; $i>1949; $i--){
-						echo "<option value=\"$i\">$i</option>\n";
-					}
-				?>
-			</select><br/>
-		<input type="submit" value="Search" name="submit" /><br/>
-	</form>
+        	
+			<?php
+		include "dbconnect.php";
+		if(isset($_GET['searchterm'])){
+			$searchTerm = mysqli_real_escape_string($db, $_GET['searchterm']);
+			$query = "SELECT * FROM videogames WHERE gamename LIKE '%$searchTerm%' ORDER BY gamename;";
+			$result = mysqli_query($db, $query)
+				or die("Error Querying Database");
+		}
+		echo '<div id="gamelist">';
+		while($row = mysqli_fetch_array($result)) {
+  			$game = $row['gamename'];
+  			$id = $row['id'];
+		  	echo '<a href="showGame.php?id=' . $id . '"> - ' . $game . '</a>';
+	    }
+		echo '</div>';
+		
+	?>
+		<div>
+			Don't see the game you're looking for? 
+			<a href="addGame.php">Create a page for it!</a>
+			
+			
 			</p>
     </div>
     <br class="clearfloat" />
@@ -110,8 +85,6 @@
         </p>
     </div>
 <!-- end #footer -->
-
-
 	
 
 
