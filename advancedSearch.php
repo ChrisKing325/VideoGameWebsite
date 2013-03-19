58013aaff405da4adef734010b29c3733649cf47
Elisabeth <ena1234@hotmail.com>
2013-03-19 00:54:58 -0400

    Beginning the advanced search

--- advancedSearch.php
+++ advancedSearch.php
@@ -48,8 +48,51 @@
     	<div class="titleBlock">Advanced Search</div>
         <p>
         	<form method="get" action="searchResults.php">
-		Enter your search term here: 
-		<input type="text" id="searchterm" name="searchterm" size="40"/>
+		Enter your search terms here: <br/>
+		Name of game: <input type="text" id="name" name="name" size="40"/><br/>
+		ESRB Rating: 
+			<select id="esrb" name="esrb">
+				<option value="none">Select...</option>
+				<option value="Early Childhood">EC - Early Childhood</option>
+				<option value="Everyone 10+">E10 - Everyone 10+</option>
+				<option value="Teen">T - Teen</option>
+				<option value="Mature">M - Mature</option>
+				<option value="Adults Only">A - Adults Only</option>
+				<option value="Rating Pending">RP - Rating Pending</option>
+			</select><br/>
+		Genre: <input type="text" id="genre" name="genre" size="40"/><br/>
+		Release Date:
+			<select id="month" name="month">
+				<option value="m">Month</option>
+				<option value="1">January</option>
+				<option value="2">February</option>
+				<option value="3">March</option>
+				<option value="4">April</option>
+				<option value="5">May</option>
+				<option value="6">June</option>
+				<option value="7">July</option>
+				<option value="8">August</option>
+				<option value="9">September</option>
+				<option value="10">October</option>
+				<option value="11">November</option>
+				<option value="12">December</option>
+			</select>
+			<select id="day" name="day">
+				<option value="d">Day</option>
+				<?php
+					for($i=1; $i<32; $i++){
+						echo "<option value=\"$i\">$i</option>\n";
+					}
+				?>
+			</select>
+			<select id="year" name="year">
+				<option value="y">Year</option>
+				<?php
+					for($i=2050; $i>1949; $i--){
+						echo "<option value=\"$i\">$i</option>\n";
+					}
+				?>
+			</select><br/>
 		<input type="submit" value="Search" name="submit" /><br/>
 		blah blah blah more search info goes here
 	</form>
