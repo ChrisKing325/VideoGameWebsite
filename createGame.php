069a7e01a3dc91624f31f62b037cf52e04cad9b2
Elisabeth <ena1234@hotmail.com>
2013-03-22 16:59:52 -0400

    createGame now adds a system to the database

--- createGame.php
+++ createGame.php
@@ -50,7 +50,7 @@
 				<?php
 				include "dbconnect.php";
 				//echo $_POST["name"];
-				if($_POST["name"] != "" && $_POST["esrb"] != "none" && $_POST["genre"] != "" && $_POST["month"] != "m" && $_POST["day"] != "d" && $_POST["year"] != "y" && $_POST["piclink"] != ""){
+				if($_POST["name"] != "" && $_POST["esrb"] != "none" && $_POST["genre"] != "" && $_POST["month"] != "m" && $_POST["day"] != "d" && $_POST["year"] != "y" && $_POST["piclink"] != "" && $_POST["system"] != "sys"){
 					$name = $_POST["name"];
 					$esrb = $_POST["esrb"];
 					$genre = $_POST["genre"];
@@ -58,6 +58,7 @@
 					$day = $_POST["day"];
 					$year = $_POST["year"];
 					$piclink = $_POST["piclink"];
+					$system = $_POST["system"];
 					
 					//echo $year/4;
 					if((($month == 9 || $month == 4 || $month == 6 || $month == 11) && $day > 30) || ($month == 2 && (($year % 4 != 0 && $day > 28) || $day > 29))){
@@ -66,14 +67,19 @@
 						//at this point, everything has been filled out, and the date given is a valid date.
 						$query = 'INSERT INTO gamereviews.videogames (id, gamename, ESRBrating, genre, releasedate, score, replayability, AdminReview, picturelink) VALUES (NULL, "'.$name.'", "'.$esrb.'", "'.$genre.'", "'.$year.'-'.$month.'-'.$day.'", NULL, NULL, NULL, "'.$piclink.'");';
 						mysqli_query($db, $query)
-								or die("Error Querying Database");
-						$query = 'SELECT * FROM gamereviews.videogames WHERE gamename = "'.$name.'" AND ESRBRating = "'.$esrb.'" AND releasedate = "'.$year.'-'.$month.'-'.$day.'";';
+								or die("Error Querying Database: add game");
+						$query = 'SELECT id FROM gamereviews.videogames WHERE gamename = "'.$name.'" AND ESRBRating = "'.$esrb.'" AND releasedate = "'.$year.'-'.$month.'-'.$day.'";';
 						//echo $query;
 						$result = mysqli_query($db, $query)
-								or die("Error Querying Database");
+								or die("Error Querying Database: find game");
 						$row = mysqli_fetch_array($result);
 						$id = $row['id'];
+						//echo " The id is $id!";
 						echo 'Click <a href="showGame.php?id=' . $id . '">here</a> to go to the page you just created!';
+						$query = "INSERT INTO gamereviews.systems (id, system) VALUES($id, '$system');";
+						//echo " $query";
+						mysqli_query($db, $query)
+								or die("Error Querying Database: add system");
 						
 					}
 					
