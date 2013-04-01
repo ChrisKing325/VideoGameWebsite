65b9f6f275844dd32794065bbcfe58bb980c7e0a
Austin Wegner <awegner@mail.umw.edu>
2013-03-31 20:40:27 -0400

    fixed php error

--- gologin.php
+++ gologin.php
@@ -1,11 +1,11 @@
-<?php session_start();
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-
+<?php
 	include "dbconnect.php";
 	if($_POST['userName'] != "" && $_POST['pw'] != ""){
 		$username = $_POST['userName'];
